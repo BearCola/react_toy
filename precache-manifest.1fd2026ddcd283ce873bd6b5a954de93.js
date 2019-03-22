75ec32a560643dbb8ddc0cb601a39cf1735ac06e
Sangin <sangin.kang@navercorp.com>
2019-03-22 11:18:06 +0900

    Updates

---
+++ precache-manifest.1fd2026ddcd283ce873bd6b5a954de93.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "1bc39ceb223ddbe9378e",
+    "url": "/react_toy/static/js/runtime~main.58fe14b3.js"
+  },
+  {
+    "revision": "de9af06304c173167f9a",
+    "url": "/react_toy/static/js/main.a87a81c3.chunk.js"
+  },
+  {
+    "revision": "8c0eeccb4f0bae323dd0",
+    "url": "/react_toy/static/js/2.237338eb.chunk.js"
+  },
+  {
+    "revision": "3051e3bf524eda45aefcd0a303a6c8c9",
+    "url": "/react_toy/index.html"
+  }
+];
